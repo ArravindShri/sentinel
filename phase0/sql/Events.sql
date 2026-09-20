764f464662be9aa4701257e0e2bff526f016a2f4
Arravind Shri <shri@arravindportfolio.tech>
2026-09-20 20:16:07 +0530

    All Layer SQL Queries

---
+++ phase0/sql/Events.sql
@@ -0,0 +1,26 @@
+CREATE TABLE SENTINEL.EVENTS_NAME.Events(
+event_code STRING,
+event_name STRING 
+)
+
+INSERT INTO SENTINEL.EVENTS_NAME.Events VALUES
+('01','Make Public Statement'),
+('02','Appeal'),
+('03','Express Intent to Cooperate'),
+('04','Consult'),
+('05','Engage in Diplomatic Cooperation'),
+('06','Engage in Material Cooperation'),
+('07','Provide Aid'),
+('08','Yield'),
+('09','Investigate'),
+('10','Demand'),
+('11','Disapprove'),
+('12','Reject'),
+('13','Threaten'),
+('14','Protest'),
+('15','Exhibit force posture'),
+('16','Reduce Relations'),
+('17','Coerce'),
+('18','Assault'),
+('19','Fight'),
+('20','Use of unconventional mass violence')
